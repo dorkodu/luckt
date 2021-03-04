68be55e6544fdaae88f03c7dfe760fc5dec3a1d5
Doruk Dorkodu <dorkodu@gmail.com>
2021-03-04 12:46:29 +0300

    Wrote a simple Redux createStore clone, will simplfy it for Luckt

--- luckt.js
+++ luckt.js
@@ -28,3 +28,62 @@ function createStore(properties) {
     },
   };
 }
+
+/**
+ * TODO: MAKE THIS SIMPLE REDUX CLONE, SOMETHING REASONABLE
+ * A simplified clone of Redux : createStore
+ *  - Create a store with methods :
+ *    1- getState: returns the current state
+ *    2- subscribe: add a listener
+ *    3- unsubscribe: remove a listener
+ *    4- dispatch: takes an action and updates the state
+ */
+
+const Redux = {
+
+  createStore(reducer) {
+
+    var state;
+    var listeners = [];
+
+    function getState() {
+      return state;
+    }
+
+    function subscribe(listener) {
+
+      listeners.push(listener);
+
+      return function unsubscribe() {
+        // index of listener callback
+        let listenerIndex = listeners.indexOf(listener);
+
+        // if listener do exist, remove it from the listeners list
+        if (listenerIndex !== -1) {
+          listeners.splice(listeners.indexOf(listener), 1);
+        }
+      };
+    }
+
+    function dispatch(action) {
+
+      // generate the new state
+      state = reducer(state, action);
+
+      // call listeners
+      listeners.forEach(function (listener) {
+        listener();
+      });
+
+      // dispatch always returns the action
+      // this is really important for middleware's
+      return action;
+    }
+
+    return {
+      getState: getState,
+      subscribe: subscribe,
+      dispatch: dispatch,
+    };
+  }
+}
