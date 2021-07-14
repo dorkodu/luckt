f297c95b1aa11a16fa374fc34a36206d42af8689
Berk Cambaz <berkcambaz12321@gmail.com>
2021-07-14 14:17:47 +0300

    Add getters

--- source/luckt.js
+++ source/luckt.js
@@ -8,19 +8,28 @@ export const Luckt = {
  * @param {object} props.state
  * @param {object} [props.acts]
  * @param {object} [props.futures]
+ * @param {object} [props.getters]
  */
 function store(props) {
   const state = props.state;
   const acts = props.acts;
   const futures = props.futures;
+  const getters = props.getters;
 
   const store = {};
   store.commit = commit.bind({ state: state, acts: acts });
   store.promise = promise.bind({ commit: store.commit, futures: futures })
+  store.getters = {};
 
   Object.defineProperty(store, "state", {
     get: () => Object.assign({}, state)
-  })
+  });
+
+  Object.keys(getters).forEach((key) => {
+    Object.defineProperty(store.getters, key, {
+      get: () => getters[key](Object.assign({}, state), getters)
+    });
+  });
 
   return store;
 }
