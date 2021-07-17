cd91d83e8eedfb63415b0173da988be0b2490a78
Berk Cambaz <berkcambaz12321@gmail.com>
2021-07-17 23:03:19 +0300

    Add more code-completition support & make state exposed

--- source/luckt.js
+++ source/luckt.js
@@ -3,12 +3,47 @@ export const Luckt = {
 };
 
 /**
+ * @typedef Context
+ * @property {commit} commit
+ */
+
+/**
+ * @callback act
+ * @param {any} state
+ * @param {any} payload
+ */
+
+/**
+ * @callback future
+ * @param {Context} context
+ * @param {any} payload
+ */
+
+/**
+ * @callback commit
+ * @param {string} act
+ * @param {any} payload
+ */
+
+/**
+ * @callback promise
+ * @param {string} future
+ * @param {any} payload
+ */
+
+/**
+ * @callback getter
+ * @param {any} state
+ * @param {Object<string, getter} getters
+ */
+
+/**
  * 
  * @param {object} props 
- * @param {object} props.state
- * @param {object} [props.acts]
- * @param {object} [props.futures]
- * @param {object} [props.getters]
+ * @param {any} props.state
+ * @param {Object<string, act} [props.acts]
+ * @param {Object<string, future} [props.futures]
+ * @param {Object<string, getter} [props.getters]
  */
 function store(props) {
   const state = props.state;
@@ -20,24 +55,26 @@ function store(props) {
   store.commit = commit.bind({ state: state, acts: acts });
   store.promise = promise.bind({ commit: store.commit, futures: futures })
   store.getters = {};
+  store.state = state;
 
-  Object.defineProperty(store, "state", {
-    get: () => Object.assign({}, state)
-  });
-
-  Object.keys(getters).forEach((key) => {
+  for (const key in getters)
     Object.defineProperty(store.getters, key, {
-      get: () => getters[key](Object.assign({}, state), getters)
+      get: () => getters[key](state, getters)
     });
-  });
 
   return store;
 }
 
-function commit(act, ...args) {
-  this.acts[act](this.state, ...args);
+/** @type {commit} commit */
+function commit(act, payload) {
+  this.acts[act](this.state, payload);
 }
 
-function promise(future) {
-  this.futures[future]({ commit: this.commit });
+/** @type {promise} promise */
+function promise(future, payload) {
+  this.futures[future]({ commit: this.commit }, payload);
+}
+
+function watch() {
+
 }
